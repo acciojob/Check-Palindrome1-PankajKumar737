097fcc4771d44016bb2cf643beacf750181b4d6f
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-05 16:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+String newStr = str.split("").reverse().join("");
+	if(str === newStr){
+		return 1;
+	}
+	return 0;
 }
 module.exports = palindrome
