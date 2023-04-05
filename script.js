d6c64ffb83982e72ad71192f4e2084756dc314eb
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-05 20:20:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // complete the given function
 
+// function palindrome(str){
+// String newStr = str.split("").reverse().join("");
+// 	if(str === newStr){
+// 		return true;
+// 	}
+// 	return false;
+// }
+// //console.log(palindrome("racecar"));
+
 function palindrome(str){
-String newStr = str.split("").reverse().join("");
-	if(str == newStr){
-		return true;
-	}
-	return false;
+str=str.toLowerCase();
+let i=0;
+let j=str.length-1;
+while(i<j){
+if(str.charAt(i)==str.charAt(j)){
+i++;
+j--;
+}else{
+return false;
 }
-//console.log(palindrome("racecar"));
+}
+return true;
+
+}
+module.exports = palindrome
 module.exports = palindrome
